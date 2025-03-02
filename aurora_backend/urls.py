78e1595170a04e67c725e4dd0e7135c7c09085ca
Eldos <eldos.baktybekov@gmail.com>
2025-03-02 18:33:18 +0600

    :recycle: Move auth API routes into users app

--- aurora_backend/urls.py
+++ aurora_backend/urls.py
@@ -1,22 +1,9 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import include, path
 
 
 urlpatterns = [
-    path(f'{settings.ROOT_PATH}/admin/', admin.site.urls),
-    path(
-        f'{settings.ROOT_PATH}/api/token/',
-        TokenObtainPairView.as_view(),
-        name='access-token',
-    ),
-    path(
-        f'{settings.ROOT_PATH}/token/refresh/',
-        TokenRefreshView.as_view(),
-        name='refresh-token',
-    ),
+    path(f'{settings.ROOT_PATH}admin/', admin.site.urls),
+    path(r'{settings.ROOT_PATH}', include('users.urls')),
 ]

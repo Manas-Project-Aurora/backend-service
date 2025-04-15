381596bfdf96c619ffe0f6971f8f4562c2e5b86b
Erkin <erkint@gmail.com>
2025-04-15 18:33:02 +0600

    Add serializers for vacancy and organization retrieval

---
+++ board/serializers/organizations/retrieve.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from board.serializers.organizations.list import OrganizationContactSerializer
+
+
+class OrganizationRetrieveOutputSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    description = serializers.CharField(allow_null=True)
+    logo_url = serializers.URLField(allow_null=True)
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+    contacts = OrganizationContactSerializer(many=True)
+    vacancy_count = serializers.IntegerField()
+    event_count = serializers.IntegerField()
+    video_count = serializers.IntegerField()

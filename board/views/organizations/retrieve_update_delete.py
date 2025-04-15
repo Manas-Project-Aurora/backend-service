2c0baf1c4ca312bb1c51ba296d8bc9018b04ef14
Erkin <erkint@gmail.com>
2025-04-15 18:32:48 +0600

    Implement organization and vacancy retrieval with error handling

--- board/views/organizations/retrieve_update_delete.py
+++ board/views/organizations/retrieve_update_delete.py
@@ -2,11 +2,26 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from board.exceptions import OrganizationNotFoundError
+from board.models import Organization
+from board.serializers.organizations.retrieve import OrganizationRetrieveOutputSerializer
+
 
 class OrganizationRetrieveUpdateDeleteApi(APIView):
 
-    def get(self, request: Request) -> Response:
-        return Response()
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return []
+        return super().get_permissions()
+
+    def get(self, request: Request, organization_id: int) -> Response:
+        try:
+            organization = Organization.objects.get(id=organization_id)
+        except Organization.DoesNotExist:
+            raise OrganizationNotFoundError
+
+        serializer = OrganizationRetrieveOutputSerializer(organization)
+        return Response(serializer.data)
 
     def put(self, request: Request) -> Response:
         return Response()
--- board/views/vacancies/retrieve_update_delete.py
+++ board/views/vacancies/retrieve_update_delete.py
@@ -2,11 +2,13 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from board.exceptions import VacancyNotFoundError
 from board.models import Vacancy
 from board.serializers.vacancies.list import (
     VacancyListInputSerializer,
     VacancyListOutputSerializer,
 )
+from board.serializers.vacancies.retrieve import VacancyRetrieveOutputSerializer
 
 
 class VacancyRetrieveUpdateDeleteApi(APIView):
@@ -16,46 +18,13 @@ class VacancyRetrieveUpdateDeleteApi(APIView):
             return []
         return super().get_permissions()
 
-    def get(self, request: Request) -> Response:
-        serializer = VacancyListInputSerializer(data=request.query_params)
-        serializer.is_valid(raise_exception=True)
-        data: dict = serializer.validated_data
+    def get(self, request: Request, vacancy_id: int) -> Response:
+        try:
+            vacancy = Vacancy.objects.select_related('organization').get(id=vacancy_id)
+        except Vacancy.DoesNotExist:
+            raise VacancyNotFoundError
 
-        take: int = data['take']
-        skip: int = data['skip']
-        statuses: list[Vacancy.Status] = data['statuses']
-        types: list[Vacancy.Type] | None = data['types']
-        salary_from: int | None = data['salary_from']
-        salary_to: int | None = data['salary_to']
-        salary_types: list[Vacancy.SalaryType] | None = data['salary_types']
-
-        vacancies = Vacancy.objects.select_related('organization').filter(
-            status__in=statuses,
-        ).order_by(
-            '-created_at'
-        )
-        if types is not None:
-            vacancies = vacancies.filter(type__in=types)
-        if salary_from is not None:
-            vacancies = vacancies.filter(salary_from__gte=salary_from)
-        if salary_to is not None:
-            vacancies = vacancies.filter(salary_to__lte=salary_to)
-        if salary_types is not None:
-            vacancies = vacancies.filter(salary_type__in=salary_types)
-
-        total_count = vacancies.count()
-        vacancies = vacancies[skip: skip + take]
-
-        serializer = VacancyListOutputSerializer(
-            {
-                'vacancies': vacancies,
-                'pagination': {
-                    'total_count': total_count,
-                    'take': take,
-                    'skip': skip,
-                }
-            }
-        )
+        serializer = VacancyRetrieveOutputSerializer(vacancy)
         return Response(serializer.data)
 
     def put(self, request: Request) -> Response:

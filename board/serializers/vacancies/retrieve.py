307e9d6d4e33b56808764d0c8f434e5ca97c80d0
Erkin <erkint@gmail.com>
2025-04-15 18:33:20 +0600

    Add fields to VacancyRetrieveOutputSerializer for detailed vacancy information

--- board/serializers/vacancies/retrieve.py
+++ board/serializers/vacancies/retrieve.py
@@ -1,5 +1,21 @@
 from rest_framework import serializers
 
+from board.models import Vacancy
+
 
 class VacancyRetrieveOutputSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    organization_id = serializers.IntegerField(source='organization.id')
+    organization_name = serializers.CharField(source='organization.name')
+    salary_from = serializers.IntegerField(allow_null=True)
+    salary_to = serializers.IntegerField(allow_null=True)
+    status = serializers.ChoiceField(choices=Vacancy.Status.choices)
+    type = serializers.ChoiceField(choices=Vacancy.Type.choices)
+    salary_type = serializers.ChoiceField(choices=Vacancy.SalaryType.choices)
+    user_id = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+
+

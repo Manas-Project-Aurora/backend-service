5cf844bbd3c6ec84ccb751d33219c4a926232248
Erkin <erkint@gmail.com>
2025-04-16 20:29:44 +0600

    :sparkles: Add vacancy approval and rejection functionality with corresponding API endpoints

---
+++ board/tests/test_views/test_vacancy_reject_api.py
@@ -0,0 +1,37 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from board.models import Vacancy
+from board.tests.factories import VacancyFactory
+
+@pytest.mark.django_db
+def test_vacancy_reject_api(user, authenticated_client):
+    vacancy = VacancyFactory(status=Vacancy.Status.PENDING, user=user)
+    url = reverse('board:vacancy-reject', kwargs={'vacancy_id': vacancy.id})
+
+    response = authenticated_client.post(url)
+
+    vacancy.refresh_from_db()
+    assert vacancy.status == Vacancy.Status.REJECTED
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+@pytest.mark.django_db
+def test_vacancy_reject_if_not_admin(authenticated_not_admin):
+    vacancy = VacancyFactory(status=Vacancy.Status.PENDING)
+    url = reverse('board:vacancy-reject', kwargs={'vacancy_id': vacancy.id})
+
+    response = authenticated_not_admin.post(url)
+
+    assert response.json()['errors'][0]['code'] == 'vacancy_access_denied'
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+@pytest.mark.django_db
+def test_vacancy_reject_if_status_not_pending(user, authenticated_client):
+    vacancy = VacancyFactory(status=Vacancy.Status.ACTIVE, user=user)
+    url = reverse('board:vacancy-reject', kwargs={'vacancy_id': vacancy.id})
+
+    response = authenticated_client.post(url)
+
+    assert response.json()['errors'][0]['code'] == 'vacancy_is_not_pending'
+    assert response.status_code == status.HTTP_400_BAD_REQUEST

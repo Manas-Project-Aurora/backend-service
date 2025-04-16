f03ce554884a41c14f7843bb56aa9fc03670a9b1
Erkin <erkint@gmail.com>
2025-04-16 11:41:43 +0600

    :sparkles: Refactor delete method in VacancyRetrieveUpdateDeleteApi to use delete_vacancy service

--- board/views/vacancies/retrieve_update_delete.py
+++ board/views/vacancies/retrieve_update_delete.py
@@ -1,10 +1,12 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 from board.exceptions import VacancyNotFoundError
 from board.models import Vacancy
 from board.serializers.vacancies.retrieve import VacancyRetrieveOutputSerializer
+from board.services.vacancies import delete_vacancy
 
 
 class VacancyRetrieveUpdateDeleteApi(APIView):
@@ -26,23 +28,5 @@ class VacancyRetrieveUpdateDeleteApi(APIView):
         return Response()
 
     def delete(self, request: Request, vacancy_id: int) -> Response:
-        try:
-            vacancy = Vacancy.objects.get(id=vacancy_id)
-        except Vacancy.DoesNotExist:
-            raise VacancyNotFoundError
-
-        # if not request.user.is_authenticated:
-        #     return Response(
-        #         {"error": "Для удаления вакансии необходимо авторизоваться"},
-        #         status=401
-        #     )
-
-        if vacancy.user != request.user:
-            return Response(
-                {"error": "У вас нет прав для удаления этой вакансии"},
-                status=403
-            )
-
-        vacancy.delete()
-
-        return Response(status=204)
+        delete_vacancy(vacancy_id, request.user.id)
+        return Response(status=status.HTTP_204_NO_CONTENT)

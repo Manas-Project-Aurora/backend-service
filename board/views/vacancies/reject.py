8f02109740cbfcb1382093379435a5dd49d5214f
Erkin <erkint@gmail.com>
2025-04-16 20:29:59 +0600

    :sparkles: Implement vacancy approval and rejection functionality with API endpoints

---
+++ board/views/vacancies/reject.py
@@ -0,0 +1,12 @@
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from board.services.vacancies import reject_vacancy
+
+
+class VacancyRejectApi(APIView):
+    def post(self, request: Request, vacancy_id: int) -> Response:
+        reject_vacancy(vacancy_id, request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
